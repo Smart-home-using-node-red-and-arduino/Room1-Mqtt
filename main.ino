68d24f98baa76561063b63f1698ae10335d54705
Amin <dadgaramin96@gmail.com>
2021-06-01 13:50:15 +0430

    OTA functionality implemented!

--- main.ino
+++ main.ino
@@ -1,12 +1,22 @@
 #include "credentials.c"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <string.h>
 #include <Ticker.h> // ticker library to check mqtt connection status ( if disconnected then try to reconnect )
 
 
 #define LED 2
 #define relay D1
+
+
+// get credentials from c function
+// function is declared in credentials.c 
+struct credentials cred = getCredentials();
+
+
 
  
 WiFiClient espClient;
@@ -22,9 +32,7 @@ void setup() {
 
   check_Mqtt_connection.attach(180, checkMqttServer); // check mqtt server connection every 3 minutes (3*60 seconds)
 
-  // get credentials from c function
-  // function is declared in credentials.c 
-  struct credentials cred = getCredentials();
+
 
   // print credentials for debug purposes
   /* Serial.println("credentials");
@@ -32,24 +40,74 @@ void setup() {
   Serial.println(cred.password);
   Serial.println(cred.mqtt_host_ip);
   Serial.println(cred.mqtt_port);  */
- 
+
+  // setup OTA
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
   WiFi.begin(cred.ssid, cred.password);
  
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Connecting to WiFi..");
-    digitalWrite(LED,LOW);
-    delay(300);
-    digitalWrite(LED,HIGH);
-    delay(300);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
   }
-  Serial.print("Connected to WiFi :");
-  Serial.println(WiFi.SSID());
+  
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+//  Serial.println(WiFi.SSID());  // print for debug 
   digitalWrite(LED,LOW);
  
   client.setServer(cred.mqtt_host_ip, cred.mqtt_port);
   client.setCallback(mqtt_callback);
  
   while (!client.connected()) {
+    digitalWrite(LED,HIGH);
     Serial.println("Connecting to MQTT...");
 
     if (client.connect("nodeMcu")) {
@@ -57,10 +115,13 @@ void setup() {
       Serial.println("connected");  
  
     } else {
- 
+      digitalWrite(LED,LOW);
+      
       Serial.print("failed with state ");
       Serial.println(client.state());  //If you get state 5: mismatch in configuration
-      delay(2000);
+      delay(500);
+      digitalWrite(LED,HIGH);
+      delay(500);
  
     }
   }
@@ -136,5 +197,6 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 }
  
 void loop() {
+  ArduinoOTA.handle();
   client.loop();
 }
